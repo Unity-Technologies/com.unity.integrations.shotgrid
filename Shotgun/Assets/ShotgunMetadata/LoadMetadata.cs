40324f2dcf38a08990414afb13eba34e51d9104d
vkovec <viktoria.kovecses@gmail.com>
2019-02-06 13:02:41 -0500

    add code to parse shotgun metadata to load specific scene

---
+++ Shotgun/Assets/ShotgunMetadata/LoadMetadata.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Scripting.Python;
+
+[InitializeOnLoad]
+public static class LoadMetadata
+{
+    static LoadMetadata()
+    {
+        EditorApplication.update += RunPythonScript;
+    }
+
+    private static void RunPythonScript()
+    {
+        EditorApplication.update -= RunPythonScript;
+        PythonRunner.RunFile(System.IO.Path.Combine(Application.dataPath, "ShotgunMetadata/parseMetadata.py"));
+    }
+}
---
+++ Shotgun/Assets/ShotgunMetadata/parseMetadata.py
@@ -0,0 +1,24 @@
+import os
+import json
+import UnityEditor
+
+def parseMetadata():
+    # get meta data from the environment variable
+    unity_metadata_json = os.environ.get('SHOTGUN_UNITY_METADATA')
+    if not unity_metadata_json:
+        return
+    
+    unity_metadata = None
+    try:
+        unity_metadata = json.loads(unity_metadata_json)
+    except Exception, e:
+        self.logger.warning('Exception while parsing the sg_unity_metadata field: {}. The JSON data is probably invalid and will be ignored'.format(e))
+        return
+        
+    if not unity_metadata or not unity_metadata.has_key('scenePath'):
+        return
+    
+    # open the correct scene in Unity
+    UnityEditor.SceneManagement.EditorSceneManager.OpenScene(unity_metadata["scenePath"])
+
+parseMetadata()
