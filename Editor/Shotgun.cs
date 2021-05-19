48f8b19ef68404a9c62515fd94b28294f33cd4a7
mojibola obadimeji <mojibola.obadimeji@imaginary-spaces.com>
2021-05-19 13:29:20 -0400

    Proper text for summary docstring

--- Editor/Shotgun.cs
+++ Editor/Shotgun.cs
@@ -225,7 +225,7 @@ namespace UnityEditor.Integrations.Shotgun
     public static class Service
     {
         /// <summary>
-        /// Calls a service on the Shotgun client.
+        /// Executes a menu item defined in the Shotgun client.
         /// </summary>
         /// <param name="serviceName">The name of the service</param>
         /// <param name="args">Arguments to pass to the service</param>
